043c832525a8b2cf14eef412d4a3c0c3282bb4ed
Имя Фамилия <alexandrmekhaev@yandex.ru>
2025-06-06 22:48:41 +0300

    corrections

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,19 @@
 # flake8: noqa
 import os
+
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+load_dotenv()
 
-DEBUG = False
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
-ALLOWED_HOSTS = ['89.169.170.190', '127.0.0.1', 'localhost', 'sashamehaev-kittygr.bounceme.net']
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
